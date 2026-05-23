0adb63f21bf5542353e29fdcf78e3d030ac58538
infopiyushbharti-sudo <info.piyushbharti@gmail.com>
2026-05-23 15:43:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -547,21 +547,28 @@ async def upload(bot: Client, m: Message):
     await editable.edit("𝗡𝗼𝘄 𝗦𝗲𝗻𝗱 𝗧𝗵𝗲 𝗧𝗵𝘂𝗺𝗯 𝗨𝗿𝗹 𝗘𝗴 » https://graph.org/file/13a89d77002442255efad-989ac290c1b3f13b44.jpg\n\n𝗢𝗿 𝗜𝗳 𝗗𝗼𝗻'𝘁 𝗪𝗮𝗻𝘁 𝗧𝗵𝘂𝗺𝗯𝗻𝗮𝗶𝗹 𝗦𝗲𝗻𝗱 = 𝗻𝗼")
     input6 = message = await bot.listen(editable.chat.id)
     raw_text6 = input6.text
-    await input6.delete(True)
+       await input6.delete(True)
     await editable.delete()
 
-    #thumb = input6.text
-    #if thumb.startswith("http://") or thumb.startswith("https://"):
-        #getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
-        #thumb = "thumb.jpg"
+    # Thumbnail logic - commented but properly structured
+    thumb = input6.text
+    ENABLE_THUMB_DOWNLOAD = False  # ← Flag se control karein
+    
+    if ENABLE_THUMB_DOWNLOAD and (thumb.startswith("http://") or thumb.startswith("https://")):
+        getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
+        thumb = "thumb.jpg"
     else:
-        thumb == "no"
-    failed_count =0
+        thumb = "no"
+
+    failed_count = 0
+    
     if len(links) == 1:
         count = 1
     else:
-        count = int(raw_text)
-
+        try:
+            count = int(raw_text)
+        except (ValueError, TypeError):
+            count = len(links)
     try:
         for i in range(count - 1, len(links)):
             V = links[i][1].replace("file/d/","uc?export=download&id=").replace("www.youtube-nocookie.com/embed", "youtu.be").replace("?modestbranding=1", "").replace("/view?usp=sharing","") # .replace("mpd","m3u8")
